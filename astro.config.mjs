5c1ba8d14cae7f3c8d7de8aeb42b9d5ca15f346c
Brian Perry <contact.brianperry@gmail.com>
2023-12-13 08:10:45 -0600

    Redirect category pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	redirects: {
+		'/all': '/posts',
+		'/til': '/posts',
+		'/jams': '/posts',
+	  }
 });
