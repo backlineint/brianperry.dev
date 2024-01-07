b74c6528ceecb0f021ed58338742ccde1be2613d
Brian Perry <contact.brianperry@gmail.com>
2024-01-07 14:49:26 -0600

    Enable Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from "@astrojs/partytown";
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://brianperry.dev',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(), 
+		sitemap(), 
+		partytown({
+			// Adds dataLayer.push as a forwarding-event.
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		})
+	],
 	redirects: {
 		'/all': '/posts',
 		'/til': '/posts',
